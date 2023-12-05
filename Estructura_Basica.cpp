b2dade51ae724736b1e13aac35c6432b3bc2d20c
IkerFeda <iferndac@myuax.com>
2023-12-05 10:52:49 +0100

    .

---
+++ Estructura_Basica.cpp
@@ -0,0 +1,16 @@
+#include "map"
+#include "string"
+
+
+class Environment {
+private:
+    std::map<std::string, int> symbolTable;
+
+public:
+
+    Environment() {
+        symbolTable = std::map<std::string, int>();
+    }
+
+
+};
